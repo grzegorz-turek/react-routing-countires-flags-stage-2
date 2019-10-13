922bf730687a7de451be9ea977801ddd7aff7732
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-10-13 16:59:20 +0200

    Add project stage 2

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
 import DevTools from './DevTools';
 import { getCountries } from './actions/actions';
-
+import './country.css';
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes}>
+            </Router>
             <DevTools />
         </div>
     </Provider>,
     document.getElementById('root')
 )
 
-store.dispatch(getCountries());
-
-//-------------------- z create-react-app --------------------
-//ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+store.dispatch(getCountries()); // testowy dispatch akcji getCountries
--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,5 +1,25 @@
 import {GET_COUNTRIES, GET_COUNTRY, DELETE_COUNTRY, SEARCH_COUNTRIES, SET_CONTINENT} from '../actions/actions';
-const countriesData = require('../data/countries.json');// import mi nie działa, tylko require
+import countriesData from '../data/countries.json';
+
+/*
+//ja bym zrobił tak, wówczas state odzwiercidlałby plik z danymi wejściowymi src/data/countries.json
+const countriesReducer = (state = countriesData, action) => {
+    switch(action.type) {
+        case GET_COUNTRIES:
+            return state;
+        case GET_COUNTRY:
+            return state.filter(country => country.id === action.id);
+        case DELETE_COUNTRY:
+            return state.filter(country => country.id !== action.id);
+        case SEARCH_COUNTRIES:
+            return state.filter(country => (country.name).toLowerCase().includes((action.searchText).toLowerCase()));
+        case SET_CONTINENT:
+            return state.filter(country => country.continent === action.continent);
+        default:
+            return state;
+    }
+}
+*/
 
 const initialState = {
     countries: countriesData
@@ -21,25 +41,5 @@ const countriesReducer = (state = initialState, action) => {
             return state;
     }
 }
-
-/*
-//ja bym zrobił tak, wówczas state odzwiercidlałby plik z danymi wejściowymi src/data/countries.json
-const countriesReducer = (state = countriesData, action) => {
-    switch(action.type) {
-        case GET_COUNTRIES:
-            return state;
-        case GET_COUNTRY:
-            return state.filter(country => country.id === action.id);
-        case DELETE_COUNTRY:
-            return state.filter(country => country.id !== action.id);
-        case SEARCH_COUNTRIES:
-            return state.filter(country => (country.name).toLowerCase().includes((action.searchText).toLowerCase()));
-        case SET_CONTINENT:
-            return state.filter(country => country.continent === action.continent);
-        default:
-            return state;
-    }
-}
-*/
 
 export default countriesReducer;
